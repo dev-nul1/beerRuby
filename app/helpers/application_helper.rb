b51c6d5dcf77682b13f39f9636a5a4563cc1d482
Philip Scheid <hello@philipscheid.com>
2014-07-07 07:49:27 -0700

    gravatar: bug
    needs to be fixed/ add user management and more things.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
-
+ #<%= image_tag avatar_url(user) %>
+ #
+def avatar_url(user)
+  if user.avatar_url.present?
+    user.avatar_url
+  else
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  end
+end
+  	
   def flash_class(level)
     case level
       when 'notice' then "alert alert-info"
